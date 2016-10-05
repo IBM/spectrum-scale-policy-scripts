97d5dcb264cdb93da5313dfe94a7467ab2289c1d
Nils Haustein <nils_haustein@de.ibm.com>
2016-10-05 08:18:12 -0400

    Show number of selected files by default. New option -v to show file names New option -s <workDir> to set the directory for the policy engine outout

--- list.sh
+++ list.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+
 #
 # flexible framework to run list policies
 # - create a list policy where the external rule name is identical to the op-code given to this program
@@ -6,52 +7,96 @@
 # - the policy file must include the op-code in the filename: listpol_<op-code>.txt
 # - this policy file is stored in the path configurable below ($pfPrefix)
 # - run the program with the state: list.sh <state> <fspath>
+# - optional argument -v allows to show verbose output (file names)
+# - optional argument -s <workDir> allows to set the working directory for the policy engine output files
 #
 
 # ADJUST define pathname and prefix for policy files
 # policy file must be named: $pfPrefix_$op.txt
 pfPrefix="./listpol"
 
+# define local work directory for mmapplypolicy and output files, default is /tmp
+workDir="/tmp"
+
 # ADJUST define file system name
-fsName="/ibm/hsmdiv"
+fsName=""
 
-#define prefix for policy output file
-#policy output file is named: $ofPrefix.list.$op
-ofPrefix="/tmp/gpfs"
-
-#define logfile name
-logfile="/tmp/gpfs_mmapply.log"
+# define output format: 0 - number of files only; 1 - filenames
+numbersOnly=1
 
 
 #function syntax
 function syntax 
 {
   echo 
-  echo "Syntax: list state filesystem"
+  echo "Syntax: list state filesystem [-v -s workDir]"
   echo "This program lists the files according to the HSM state, Valid states are:"
-  echo "  mig:    list all migrated files"
-  echo "  pmig:   list all premigrated files"
-  echo "  res:    list all resident files"
-  echo "  all:    provides statistic about all states"
+  echo "  mig:        list all migrated files"
+  echo "  pmig:       list all premigrated files"
+  echo "  res:        list all resident files"
+  echo "  all:        provides statistic about all states"
   echo 
-  exit 1
+  echo "  Filesystem: is the name of the file system or directory"
+  echo "  -v:         shows the file names selected by the policy (default is number of files)"
+  echo "  -s workDir: specify the working directory for the policy engine output files (default is $workDir)"
+  echo 
 }
 
-#check arguments
+#check first argument to be state
 if [[ -z "$1" ]]; 
 then
+  echo "Error: state not specified."
   syntax
   exit 1
 else
   op=$1
 fi
 
+# second argument must be file system path
 if [[ -z "$2" ]]; 
 then
-  echo "Setting file system to default name: $fsName"
+  echo "Error: file system not specified"
+  syntax
+  exit 1
 else
   fsName=$2
+  if [[ ! -d $fsName ]];
+  then
+    echo "Error: file system $fsName does not exist."
+	syntax
+	exit 1
+  fi
 fi
+
+# now parse the rest of the arguments
+shift 2
+# echo "DEBUG: args=$*"
+
+while [[ ! -z "$1" ]];
+do
+  case "$1" in
+  "-v") numbersOnly=0;;
+  "-s") shift 1
+        workDir=$1
+		if [[ ! -d "$workDir" ]];
+		then 
+		   echo "Error: working directory specified by -s $workDir does not exist."
+		   syntax
+		   exit 1
+		fi ;;
+  *)    echo "Error: Unknown Argument received ($1). "
+        syntax
+		exit 1;;
+  esac
+  shift 1
+done
+
+#define prefix for policy output file
+#policy output file is named: $ofPrefix.list.$op
+ofPrefix="$workDir""/gpfs"
+
+#define logfile name
+logfile="$workDir""/gpfslist_mmapply.log"
 
 
 #check if policy file exists
@@ -59,7 +104,7 @@ polfile="$pfPrefix""_""$op"".txt"
 # echo "DEBUG: policy file name is: $polfile"
 if [[ ! -a $polfile ]];
 then
-  echo "ERROR: Policyfile $polfile does not exist in $pfPrefix. Provide the file or use correct status."
+  echo "ERROR: Policyfile $polfile does not exist in $pfPrefix. Provide the file or use correct state."
   syntax
   exit 1
 fi
@@ -74,7 +119,7 @@ done
 # if state=all then run run mmapplypolicy and subsequently determine the number of respective files
 
 # run mmapplypolicy
-mmapplypolicy $fsName -P $polfile -f $ofPrefix -I defer > $logfile
+mmapplypolicy $fsName -P $polfile -s $workDir -f $ofPrefix -I defer > $logfile
 rc=$?
 echo "=============================================================================="
 if (( rc == 0 )); 
@@ -91,13 +136,22 @@ then
 		else
 		   num=$(wc -l $outfile | awk '{print $1}')
 		fi
-		echo "  Number of files with state $s:  $num"
+		echo "  Number of files with state $s:  $num  (filename: $outfile)"
 	 done
   else
     #create name of policy output file
     outfile="$ofPrefix"".""list"".""$op"
     # echo "DEBUG: out file name is: $outfile"
-    cat $outfile;
+	if (( numbersOnly ));
+	then 
+	  num=$(wc -l $outfile | awk '{print $1}')
+	  echo "  Number of files with state $op:  $num  (filename: $outfile)"
+	else
+      cat $outfile
+	  echo "---------------------------------------------------------------------------"
+	  echo "INFO: See file $outfile"
+	  echo "---------------------------------------------------------------------------"
+	fi
   fi
 else
   echo "ERROR: mmapplypolicy returned error (rc=$rc), check log ($logfile)"
