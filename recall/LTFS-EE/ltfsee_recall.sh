8a6efd81d54a9ea5c08bf8807d254eed5e9f6657
Nils Haustein <nils_haustein@de.ibm.com>
2017-09-05 12:13:49 +0200

    Create ltfsee_recall.sh

---
+++ recall/LTFS-EE/ltfsee_recall.sh
@@ -0,0 +1,102 @@
+#! /bin/ksh
+#
+# Copy Right IBM Corporation 2017
+#
+# Program: ltfsee_recall
+#
+# Description: 
+# Interface script for MIGRATE policy invoked by mmapplypolicy 
+# Invokes ltfsee recall -l libname filelist
+#
+# Prerequisite:
+# EXTERNAL pool policy that identifies files to be recalled.
+#
+# Input:
+# invoked by mmapplypolicy with the following parameters:
+#   $1 operation (migrate, test)
+#   $2 file system name or name of filelist
+#   $3 library name
+#
+# Output:
+# Writes runtime information to STDOUT - ends up in mmapplypolicy output
+#
+# Example Policy:
+# /* define path name pattern for the file to be recalled */
+# define(recall_dir, (PATH_NAME LIKE '%'))
+# /* define external pool with customized interface script and target library for recall*/
+# RULE 'extpool' EXTERNAL POOL 'ltfs' EXEC '/path/ltfsee_recall.sh' OPTS 'eelib1'
+# /* define migration rule to recall all migrated files matching the path pattern*/
+# RULE 'recall' MIGRATE FROM POOL 'ltfs' TO POOL 'system' FOR FILESET ('swr') WHERE 
+#(XATTR('dmapi.IBMPMig') IS NULL) AND NOT (XATTR('dmapi.IBMTPS') IS NULL) and (recall_dir)
+#
+# Invokation:
+# mmapplypolicy fsname -P policyfile
+#
+# Change History
+# 10/09/12 first implementation based GAD startbackup
+# 12/20/15 implementation for immutability, some streamlining of existing code
+# 12/21/15 create the general receiver
+# 09/01/17 ltfsee recall
+
+#global variables for this script
+# set the default option in case $3 is not give, allows to specify the library name
+DEFOPTS=""
+# define ltfsee directory
+LTFSEEDIR=/opt/ibm/ltfsee/bin
+
+#++++++++++++++++++++++++++ MAIN ++++++++++++++++++++++++++++++++++++++
+echo "================================================================================================"
+echo "$(date +"%Y-%b-%d %H:%M:%S") ltfsee_recall.sh invoked with arguments: $*"
+
+## Parse Arguments & execute
+#$1 is the policy operation (list, migrate, etc) 
+op=$1
+#$2 is the policy file name
+polFile=$2
+#$3 is the option given in the EXTERNAL LIST rule with OPTS '..' should be the pool 
+shift 2
+option=$*
+    
+## evaluate the operation passed by mmapplypolicy and act upon it
+case $op in 
+  # there will always be a TEST call with $2 being the file system path
+  TEST ) 
+       echo "INFO: TEST option received for $polFile"
+	   if [[ ! -z "$polFile" ]] then
+	     if [[ ! -d "$polFile" ]] then
+		   echo "WARNING: TEST directory $polFile does not exists."
+		 fi
+	   fi
+	   ;;
+  RECALL | MIGRATE | LIST )
+  # this is the actual migrate call where we call ltfsee premigrate
+       echo "INFO: $op option received with file name $polFile and options $option"
+       #set option to default if not set
+       if [[ -z $option ]] then 
+	     if [[ -z $DEFOPTS ]] then
+		   echo "ERROR: Library name not specified in the external pool rule."
+		   exit 1
+		 else
+	      option=$DEFOPTS
+		 fi
+	   fi
+	   
+	   echo "INFO: Start processing files $polFile with ltfsee recall from library $option"
+	   $LTFSEEDIR/ltfsee recall -l $option $polFile  
+	   rc=$?
+       if (( rc != 0 )) then
+         echo "WARNING: ltfsee recall ended with return code $rc"	
+       fi		 
+       ;;
+  REDO )
+	   echo "INFO: REDO option received with file name $polFile and options $option"
+       ;;
+  * )
+	   echo "WARNING: UNKNOWN operation ($op) received with file name $polFile and options $option"
+       ;;
+esac
+
+echo "$(date +"%Y-%b-%d %H:%M:%S") ltfsee_recall ended"
+echo 
+exit 0
+
